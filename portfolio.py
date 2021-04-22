96d9732df56d493d90cf0d8bfc80fcb95545f395
bisalgt <bisalgt@gmail.com>
2021-04-22 08:32:33 +0545

    initial use the flask frozen

--- portfolio.py
+++ portfolio.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, url_for, request, redirect
 import logging
-
+import os
 
 app = Flask(__name__)
+app.config['FREEZER_BASE_URL'] = 'http://localhost/build'
+
 logging.basicConfig(filename='flaskr.log', encoding='utf-8', level=logging.DEBUG)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    print(url_for('home'))
     if request.method=='POST':
         name=request.form['name']
         email=request.form['email']
@@ -19,3 +22,5 @@ def home():
     return render_template('home.html')
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
